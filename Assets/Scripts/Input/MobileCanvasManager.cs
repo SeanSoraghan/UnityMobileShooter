64e164b5ce8b6b2d1bc26417c015f050b757113c
SeanSoraghan <sean@roli.com>
2016-12-17 17:39:02 +0000

    added input plane on touch events

--- Assets/Scripts/Input/MobileCanvasManager.cs
+++ Assets/Scripts/Input/MobileCanvasManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class MobileCanvasManager : CanvasTouchManager {
 
+    public float  InputRefreshRate        = 0.2f;
+    private float previousInputUpdateTime = 0.0f;
+
     public static bool IsTagValidTarget (string tag)
     {
         return tag == EnemyController.EnemyTag;
@@ -12,35 +15,55 @@ public class MobileCanvasManager : CanvasTouchManager {
 
     private RaycastHit raycastResult;
 
-    public override void HandleMouseDownEvent (Vector2 mousePosition) { UpdatePlayerTargetFromScreenPosition (Input.mousePosition); }
-    public override void HandleMouseDragEvent (Vector2 mousePosition) { UpdatePlayerTargetFromScreenPosition (Input.mousePosition); }
+    public override void HandleMouseDownEvent (Vector2 mousePosition) { UpdatePlayerTargetFromScreenInputPosition (Input.mousePosition); }
+    public override void HandleMouseDragEvent (Vector2 mousePosition) { UpdatePlayerTargetFromScreenInputPosition (Input.mousePosition); }
 
     public override void HandleNewOrExistingTouch (Touch t)
     {
         base.HandleNewOrExistingTouch (t);
-        UpdatePlayerTargetFromScreenPosition (t.position);
+        UpdatePlayerTargetFromScreenInputPosition (t.position);
     }
 
-    private void UpdatePlayerTargetFromScreenPosition (Vector2 screenPosition)
+    private bool ScreenPointChangesPlayerTarget (Vector2 screenPoint, PlayerController controller)
     {
-        Vector2[] southWestNorthEastInputPlane = SMath.CreatePlaneFromPoint (screenPosition, 10.0f, 10.0f);
-        if (PlayerObject != null)
-        { 
-            PlayerController controller = PlayerObject.GetComponent<PlayerController>();
-            Ray ray = Camera.main.ScreenPointToRay (screenPosition);
-            if (Physics.Raycast (ray, out raycastResult))
-            {
-                if (IsTagValidTarget (raycastResult.collider.gameObject.tag))
+        
+        Ray ray = Camera.main.ScreenPointToRay (screenPoint);
+        if (Physics.Raycast (ray, out raycastResult))
+        {
+            if (IsTagValidTarget (raycastResult.collider.gameObject.tag))
+            { 
+                if (controller != null)
                 { 
-                    if (controller != null)
-                    { 
-                        controller.UpdatePlayerTargetPosition (raycastResult.collider.gameObject);
-                        return;
-                    }
+                    controller.UpdatePlayerTargetPosition (raycastResult.collider.gameObject);
+                    return true;
                 }
             }
-            if (controller != null)
-                controller.ClearTargetPosition();
+        }
+
+        return false;
+    }
+
+    private void UpdatePlayerTargetFromScreenInputPosition (Vector2 screenPosition)
+    {
+        if (Time.time - previousInputUpdateTime > InputRefreshRate)
+        { 
+            float inputPlaneWidth  = 40.0f;
+            float inputPlaneHeight = 40.0f;
+            Vector2[] inputPoints = SMath.GetDiscretePlanePointsFromCentrePoint (screenPosition, inputPlaneWidth, inputPlaneHeight, 10);
+            if (PlayerObject != null)
+            { 
+                PlayerController controller = PlayerObject.GetComponent<PlayerController>();
+                Vector2 screenPoint = screenPosition;
+                for (int i = 0; i < inputPoints.Length; ++i)
+                { 
+                    if (ScreenPointChangesPlayerTarget (inputPoints[i], controller))
+                        return;
+                }
+
+                if (controller != null)
+                    controller.ClearTargetPosition();
+            }
+            previousInputUpdateTime = Time.time;
         }
     }
 }
--- Assets/Scripts/Utils/SMath.cs
+++ Assets/Scripts/Utils/SMath.cs
@@ -1,18 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Assertions;
 
 public class SMath
 {
-    public static Vector2[] CreatePlaneFromPoint (Vector2 Point, float width, float height)
+    public static Vector2[] CreatePlaneFromPoint (Vector2 CentrePoint, float width, float height)
     {
         Vector2[] southWestNorthEast = new Vector2[2];
         float rW     = width * 0.5f;
         float rH     = height * 0.5f;
-        float xWest  = Point.x - rW;
-        float xEast  = Point.x + rW;
-        float ySouth = Point.y - rH;
-        float yNorth = Point.y + rH;
+        float xWest  = CentrePoint.x - rW;
+        float xEast  = CentrePoint.x + rW;
+        float ySouth = CentrePoint.y - rH;
+        float yNorth = CentrePoint.y + rH;
         return new Vector2[2] { new Vector2 (xWest, ySouth), new Vector2 (xEast, yNorth) };
     }
 
+    public static Vector2[] GetDiscretePlanePointsFromCentrePoint (Vector2 CentrePoint, float planeWidth, float planeHeight, uint numDivisions)
+    {
+        int numPoints = (int) Mathf.Pow (numDivisions + 2.0f, 2.0f) + (numDivisions == 0 ? 1 : 0); // need to add the centre point if the points only include the four planar corners
+        float incrementX = planeWidth  / (numDivisions == 0 ? 1 : numDivisions);
+        float incrementY = planeHeight / (numDivisions == 0 ? 1 : numDivisions);
+
+        Vector2[] discretePlanePoints = new Vector2[numPoints];
+        Vector2[] southWestNorthEast = CreatePlaneFromPoint (CentrePoint, planeWidth, planeHeight);
+
+        bool oddNumPoints = numPoints % 2 == 1;
+
+        // begin in the centre of the plane and loop clockwise starting west, expanding out to fill the points array.
+        // filling the array this way means that it can be indexed from 0 - N following the loop, from the centre out.
+        Vector2 currentPosition = CentrePoint;
+        int pointIndex = 0;
+        discretePlanePoints[pointIndex]  = currentPosition;
+
+        currentPosition.x -= incrementX * (oddNumPoints ? 1.0f : 0.5f);
+        currentPosition.y += incrementY * (oddNumPoints ? 1.0f : 0.5f);
+
+        uint numLoops = (numDivisions / 2) + (oddNumPoints ? (uint)1 : 0);
+
+        for (int loopIndex = 0; loopIndex < numLoops; ++loopIndex)
+        { 
+            int numIncrementsInCurrentLoop = (loopIndex + 1) * 2 - (oddNumPoints ? 0 : 1);
+            Vector2 loopStartingPosition  = currentPosition;
+            for (int direction = 0; direction < 4; ++direction)
+            { 
+                for (int p = 0; p < numIncrementsInCurrentLoop; ++p)
+                {
+                    switch (direction)
+                    {
+                        case 0: currentPosition.x += incrementX; break; // loop east
+                        case 1: currentPosition.y -= incrementY; break; // loop south
+                        case 2: currentPosition.x -= incrementX; break; // loop west
+                        case 3: currentPosition.y += incrementY; break; // loop north
+                    }
+                    discretePlanePoints[++pointIndex] = currentPosition;
+                }
+            }
+            currentPosition = loopStartingPosition;
+            currentPosition.x -= incrementX;
+            currentPosition.y += incrementY;
+        }
+
+        return discretePlanePoints;
+    }
 }
